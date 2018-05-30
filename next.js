6629b2d25e38a7e4d59b5cb37b76d9cbb3bbc6ef
Alima Garcia Stéphane <36844486+garciastephane@users.noreply.github.com>
2018-05-30 15:09:38 +0200

    Update next.js

--- next.js
+++ next.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/superMiddleware', (req, res, next) => {
     console.log("hello middleware !")
     next()
 }, (req, res) => {
